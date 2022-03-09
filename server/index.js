fbf3b6d6f41a6768705fdbf78ad37c09fd18e065
pratik753 <60500551+pratik753@users.noreply.github.com>
2022-03-09 17:11:14 +0530

    big fix

--- server/index.js
+++ server/index.js
@@ -34,6 +34,7 @@ const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
 /*
 process.on('unhandledRejection', err => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
