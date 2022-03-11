5646d7c1d7164b8fde6262c99126c0742a7a00cc
pratik753 <60500551+pratik753@users.noreply.github.com>
2022-03-11 19:27:21 +0530

    add

--- server/Controller/BuyerController.js
+++ server/Controller/BuyerController.js
@@ -1,4 +1,7 @@
 const ProductDetail = require("../schema/ProductDetailSchema");
+const Buyer = require("../schema/BuyerSchema");
+const jwt = require("jsonwebtoken");
+
 exports.HomeProductName = async (req, res) => {
   try {
     const data = await ProductDetail.find().select("productName price");
@@ -13,7 +16,7 @@ exports.HomeProductName = async (req, res) => {
     });
   }
 };
-exports.B2BProductDetail = async (req, res) => {
+exports.ProductDetail = async (req, res) => {
   try {
     // maxQuantity*no of self help group
     const data = await ProductDetail.findById(req.params.productId).select(
@@ -30,20 +33,94 @@ exports.B2BProductDetail = async (req, res) => {
     });
   }
 };
-exports.B2CProductDetail = async (req, res) => {
+exports.BuyerInfo = async (req, res) => {
   try {
-    // minQuantity
-    const data = await ProductDetail.findById(req.params.productId).select(
-      "productName price minQuantity"
-    );
+    const data = await Buyer.findById(req.params.buyerId);
     // console.log(data, "data");
     res.status(200).json({ data: data, msg: "successful get" });
   } catch (err) {
     console.log(err);
     res.status(409).json({
-      status: "Failure in  get",
+      status: "Failure in  Insertion",
 
       // res.send(userMap);
     });
   }
 };
+exports.BuyerRegister = async (req, res) => {
+  const { name, email, password, companyName, companyAddress } = req.body;
+
+  try {
+    const existingUser = await Buyer.findOne({ email });
+    console.log(existingUser);
+    if (existingUser !== null) {
+      console.log("12");
+      return res.status(400).json({ message: "User already exists." });
+    }
+    // maxQuantity*no of self help group
+    const data = await Buyer.create({
+      name,
+      email,
+      password,
+      companyName,
+      companyAddress,
+    });
+    // console.log(data, "data");
+    res.status(200).json({ data: data, msg: "successful get" });
+  } catch (err) {
+    console.log(err);
+    res.status(409).json({
+      status: "Failure in  Insertion",
+
+      // res.send(userMap);
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await Buyer.findOne({ email });
+    if (!existingUser)
+      return res.status(400).json({ message: "User donot exist.", status: 1 });
+
+    if (password !== existingUser.password)
+      return res
+        .status(400)
+        .json({ message: "Invalid password or Email", status: 1 });
+
+    const token = jwt.sign(
+      {
+        email: existingUser.email,
+        id: existingUser._id,
+      },
+      "test",
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({
+      result: existingUser,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "SomeThing went wrong." });
+  }
+};
+
+// exports.B2CProductDetail = async (req, res) => {
+//   try {
+//     // minQuantity
+//     const data = await ProductDetail.findById(req.params.productId).select(
+//       "productName price minQuantity"
+//     );
+//     // console.log(data, "data");
+//     res.status(200).json({ data: data, msg: "successful get" });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(409).json({
+//       status: "Failure in  get",
+
+//       // res.send(userMap);
+//     });
+//   }
+// };
--- server/Routes/buyerRoute.js
+++ server/Routes/buyerRoute.js
@@ -3,10 +3,11 @@ const route = express.Router();
 
 const user = require("../Controller/UserController");
 const Buyer = require("../Controller/BuyerController");
-route.post("/login", user.login);
-route.post("/signup", user.signup);
+route.post("/login", Buyer.login);
+route.post("/register", Buyer.BuyerRegister);
+route.get("/BuyerInfo/:buyerId", Buyer.BuyerInfo);
 route.get("/home", Buyer.HomeProductName);
-route.get("/home/b2b/:productId", Buyer.B2BProductDetail);
-route.get("/home/b2c/:productId", Buyer.B2CProductDetail);
+route.get("/home/productDetail/:productId", Buyer.ProductDetail);
+// route.get("/home/b2c/:productId", Buyer.B2CProductDetail);
 
 module.exports = route;
